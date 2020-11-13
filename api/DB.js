69194108021d3694af7edcb21f0c26064967688d
Shai Zur <DORON-LAP@doronz-lap.shohamserver.local>
2020-11-13 17:16:26 +0200

    fourth commit

--- api/DB.js
+++ api/DB.js
@@ -1,4 +1,4 @@
 module.exports = {
     //DB: 'mongodb://localhost:27017/SHOP&DROP'
-    DB: 'mongodb://shaizur1:shaizur1@cluster0.d9fhs.mongodb.net/SHOP&DROP'
+    DB: 'mongodb+srv://shaizur1:shaizur1@cluster0.d9fhs.mongodb.net/SHOP&DROP?retryWrites=true&w=majority'
 }
